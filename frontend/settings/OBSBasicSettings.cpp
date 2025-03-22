b36fa1a8d92c1649ddc721bda4f15b67abd56c05
WitheringAway <124115470+notwithering@users.noreply.github.com>
2025-03-22 19:18:36 -0400

    do

--- frontend/settings/OBSBasicSettings.cpp
+++ frontend/settings/OBSBasicSettings.cpp
@@ -1,17 +1,17 @@
 /******************************************************************************
  Copyright (C) 2023 by Lain Bailey <lain@obsproject.com>
  Philippe Groarke <philippe.groarke@gmail.com>
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 2 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -58,12 +58,14 @@ extern bool cef_js_avail;
 
 static inline bool ResTooHigh(uint32_t cx, uint32_t cy)
 {
-	return cx > 16384 || cy > 16384;
+	// return cx > 16384 || cy > 16384;
+	return false;
 }
 
 static inline bool ResTooLow(uint32_t cx, uint32_t cy)
 {
-	return cx < 32 || cy < 32;
+	// return cx < 32 || cy < 32;
+	return false;
 }
 
 /* parses "[width]x[height]", string, i.e. 1024x768 */
